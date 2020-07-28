c60486bacd0e25df5217990d02b1e0f6d5882f4f
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-07-28 17:38:19 -0400

    indexdb

---
+++ db/index.js
@@ -0,0 +1,18 @@
+//require connection
+
+const connection = require("./connection");
+
+// create DB class and find all employees function
+
+class DB {
+  constructor(connection) {
+    this.connection = connection;
+  }
+  findAllEmployees() {
+    return this.connection.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+    );
+  }
+}
+
+module.exports = new DB(connection);
